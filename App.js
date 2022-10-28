8d823bf80f7b36a9ae0790139a10607d7a7b3839
krakenheim <j.n.krag@gmail.com>
2022-10-28 13:55:34 +0200

    Navigation and GPS added (GPS not used yet).

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import MapView from "react-native-maps";
+import ActButton from "./components/actButton";
+
+function SelectionScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Tryk på den funktion du ønsker.</Text>
+        <ActButton tech={"NFC"}></ActButton>
+        <ActButton tech={"Kamera"}></ActButton>
+        <ActButton tech={"GPS"}></ActButton>
+        <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Det burde virke nu og ligger på github</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Selection" component={SelectionScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  text: {
+    paddingBottom: 10,
   },
 });
---
+++ components/actButton.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+
+const ActButton = ({ tech }) => {
+  return (
+    <TouchableOpacity style={styles.button}>
+      <Text style={styles.text}>Aktiver {tech}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "blue",
+    width: "100%",
+    height: 70,
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 15,
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
+
+export default ActButton;
