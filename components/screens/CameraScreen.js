1e5da2990af48ac3d51f4f25d905af00e16ddab5
krakenheim <j.n.krag@gmail.com>
2022-11-04 15:55:58 +0100

    start på kamera

--- components/screens/CameraScreen.js
+++ components/screens/CameraScreen.js
@@ -1,12 +1,30 @@
 import { View, Text, StyleSheet, Dimensions, Permission } from "react-native";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Camera, CameraType } from "expo-camera";
 
 export default function CameraScreen() {
+  const [type, setType] = useState(CameraType.back);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const [image, setImage] = useState(null);
+  const cameraRef = useRef(null);
+
+  /* const [permission, requestPermission] = Camera.useCameraPermissions(); */
+  useEffect(() => {
+    async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+    };
+  }, []);
+
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+
   return (
-    <View>
-        <Text>
-            Dette er Kamera.
-        </Text>
+    <View style={styles.container}>
+      <Camera type={type}></Camera>
     </View>
   );
 }
