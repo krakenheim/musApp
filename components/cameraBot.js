fc62b12c42bb537248f2fc452f3c01ebb023175d
krakenheim <j.n.krag@gmail.com>
2022-11-24 11:55:06 +0100

    Camera working with buttons.

--- components/cameraBot.js
+++ components/cameraBot.js
@@ -1,12 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
+import React from "react";
+import { Entypo } from "@expo/vector-icons";
 
-export default function cameraBot() {
+export default function CameraBot({ title, onPress, icon, color, size}) {
   return (
-    <View>
-      <Text>cameraBot</Text>
-    </View>
-  )
+    <TouchableOpacity onPress={onPress} style={styles.button}>
+      <Entypo name={icon} size={28} color={color ? color : "#f1f1f1"} />
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  button: {
+    height: 40,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#f1f1f1',
+    marginLeft: 10,
+  },
+});
