ba2f39f3ae27707d0f6584809ee1a72401fe1018
krakenheim <j.n.krag@gmail.com>
2022-11-25 13:19:23 +0100

    Adding gestureHandlerRootView to nfc to test

--- components/screens/NfcScreen.js
+++ components/screens/NfcScreen.js
@@ -1,13 +1,14 @@
 import { View, Text, StyleSheet, Dimensions, Permission } from "react-native";
 import React, {useState, useEffect} from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function NfcScreen() {
   return (
-    <View>
-        <Text>
-            Dette er KFC... Undskyld NFC.
-        </Text>
-    </View>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text style={styles.loading}>Dette er KFC... Undskyld NFC.</Text>
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
@@ -17,5 +18,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  loading: {
+    flex: 1,
+    textAlign: "center",
+    textAlignVertical: "center",
+    justifyContent: "center",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
